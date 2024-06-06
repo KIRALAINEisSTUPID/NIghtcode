666494b0ee860076e8c11790126fa63d9375453f
KIRALAINEisSTUPID <169078556+KIRALAINEisSTUPID@users.noreply.github.com>
2024-06-07 00:47:03 +0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,30 @@
+import colorama
+
+colorama.init(autoreset=True)
+
+
+
+
+from pictures.functions import Phlyn, Feli, System, present,press_continue,press_start,Storyteller,mysterious_man,choose1
+press_start()
+present()
+press_continue()
+Feli("Brother,Brother...wake up, we must to go school")
+press_continue()
+Phlyn("Ok wait a minute")
+press_continue()
+Phlyn("I dont like a school,but i must to go School for sister.I dont want make her sad")
+press_continue()
+Storyteller("Phyln washed his hands and ate, but that dream did not leave his mind:")
+mysterious_man("DO U THink its real?")
+press_continue()
+Phlyn("Who are u?")
+press_continue()
+mysterious_man("its not important")
+press_continue()
+mysterious_man("what do u think about programm..@#$%*#(# ")
+press_continue()
+mysterious_man("its all @Y@I#Y4y5y56....")
+Phlyn("STOP,Where are you going?")
+System("stop him(yes/no):")
+choose1()
